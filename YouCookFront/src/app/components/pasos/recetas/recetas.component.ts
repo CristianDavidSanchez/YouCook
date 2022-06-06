ba06eebf1dfc25502aed404ea9128954fc0c88dc
cristians <cristians@agatadata.com>
2022-06-05 21:35:44 -0500

    feature:Segundo step

--- YouCookFront/src/app/components/pasos/recetas/recetas.component.ts
+++ YouCookFront/src/app/components/pasos/recetas/recetas.component.ts
@@ -7,7 +7,50 @@ import { Router } from '@angular/router';
   styleUrls: ['./recetas.component.css']
 })
 export class RecetasComponent implements OnInit {
-
+  products:any=[
+    {
+      id:0,
+      nombre:"primera receta",
+      descripcion : "Soy la primera receta",
+      urlImagen : "https://www.lavanguardia.com/files/image_948_465/files/fp/uploads/2020/09/09/5f58b1bb6d322.r_d.627-418.jpeg"
+    },
+    {
+      id:1,
+      nombre:"primera receta",
+      descripcion : "Soy la primera receta",
+      urlImagen : "https://www.lavanguardia.com/files/image_948_465/files/fp/uploads/2020/09/09/5f58b1bb6d322.r_d.627-418.jpeg"
+    },
+    {
+      id:2,
+      nombre:"primera receta",
+      descripcion : "Soy la primera receta",
+      urlImagen : "https://www.lavanguardia.com/files/image_948_465/files/fp/uploads/2020/09/09/5f58b1bb6d322.r_d.627-418.jpeg"
+    },
+    {
+      id:3,
+      nombre:"primera receta",
+      descripcion : "Soy la primera receta",
+      urlImagen : "https://www.lavanguardia.com/files/image_948_465/files/fp/uploads/2020/09/09/5f58b1bb6d322.r_d.627-418.jpeg"
+    },
+    {
+      id:4,
+      nombre:"primera receta",
+      descripcion : "Soy la primera receta",
+      urlImagen : "https://www.lavanguardia.com/files/image_948_465/files/fp/uploads/2020/09/09/5f58b1bb6d322.r_d.627-418.jpeg"
+    },
+    {
+      id:5,
+      nombre:"primera receta",
+      descripcion : "Soy la primera receta",
+      urlImagen : "https://www.lavanguardia.com/files/image_948_465/files/fp/uploads/2020/09/09/5f58b1bb6d322.r_d.627-418.jpeg"
+    },
+    {
+      id:5,
+      nombre:"primera receta",
+      descripcion : "Soy la primera receta",
+      urlImagen : "https://www.lavanguardia.com/files/image_948_465/files/fp/uploads/2020/09/09/5f58b1bb6d322.r_d.627-418.jpeg"
+    },
+  ]
   constructor(private router: Router) { }
 
   ngOnInit(): void {
@@ -17,5 +60,8 @@ export class RecetasComponent implements OnInit {
   }
   prevPage(){
     this.router.navigate(['proceso/plan']);
+  }
+  handleClick(event:Event){
+    console.log(event)
   }
 }
